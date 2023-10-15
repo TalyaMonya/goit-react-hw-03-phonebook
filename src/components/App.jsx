a9e61a2d536173f12b27335b2240b806d2649c22
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-10-15 16:53:12 +0300

    Added entry to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,25 @@ import { Filter } from "./Filter/Filter";
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts'); // Отримуємо дані з LocalStorage
+    const parsedContacts = JSON.parse(contacts); // Перетворюємо з строки JSON в обʼєкт JS
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts }); // Записуємо отримані дані в state
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts)); // Якщо контакти змінились, записуємо в LocalStorage
+    }
+  }
 
   // Додавання нового контакту в список котактів
   addContact = contact => {
